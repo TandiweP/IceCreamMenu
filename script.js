1d33847b3f458ff1e3c5f41cb7f187970219a6ab
TandiweP <87207713+TandiweP@users.noreply.github.com>
2022-08-18 09:20:26 -0600

    main

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 
 export default function selectIceCream () {
-const vanilla = true;
-const chocolate = false;
-const rockyRoad = true;
-const pbCup = true; 
+    // const vanilla = true;
+    // const chocolate = false;
+    // const rockyRoad = true;
+    // const pbCup = true; 
+    
+    // if (vanilla === true) {
+    //     return alert("Vanilla on the way!");
+    // } else if (chocolate === true) {
+    //     return alert("Chocolate day!");
+    // } else if (rockyRoad === true) {
+    //     return alert("Walk the Rocky Road!");
+    // } else if (pbCup === true) {
+    //     return alert("PB is my FAVORITE!!!");
+    // } else {
+    //     return alert("All out, sucka")
+    // }
+    console.log("Clicked");
+    };
 
-if (vanilla === true) {
-    return alert("Vanilla on the way!");
-} else if (chocolate === true) {
-    return alert("Chocolate day!");
-} else if (rockyRoad === true) {
-    return alert("Walk the Rocky Road!");
-} else if (pbCup === true) {
-    return alert("PB is my FAVORITE!!!");
-} else {
-    return alert("All out, sucka")
-}
 
-}
+
+
+
+
+// export default function selectIceCream () {
+// const vanilla = true;
+// const chocolate = false;
+// const rockyRoad = true;
+// const pbCup = true; 
+
+// if (vanilla === true) {
+//     return alert("Vanilla on the way!");
+// } else if (chocolate === true) {
+//     return alert("Chocolate day!");
+// } else if (rockyRoad === true) {
+//     return alert("Walk the Rocky Road!");
+// } else if (pbCup === true) {
+//     return alert("PB is my FAVORITE!!!");
+// } else {
+//     return alert("All out, sucka")
+// }
+
+// };
